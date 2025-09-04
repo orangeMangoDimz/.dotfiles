f9414ff567b02643eaeb591d40bd8f1755d53f2d
orangeMangoDimz <hakuka123osaimo456@gmail.com>
2025-09-04 22:40:05 +0700

    update yazi config theme

---
+++ yazi/.config/yazi/init.lua
@@ -0,0 +1,24 @@
+--- change number of columns in small windows
+function Tab:layout()
+    if self._area.w > 80 then
+        self._chunks =
+            ui.Layout():direction(ui.Layout.HORIZONTAL):constraints(
+                {
+                    ui.Constraint.Ratio(MANAGER.ratio.parent, MANAGER.ratio.all),
+                    ui.Constraint.Ratio(MANAGER.ratio.current, MANAGER.ratio.all),
+                    ui.Constraint.Ratio(MANAGER.ratio.preview, MANAGER.ratio.all)
+                }
+            ):split(self._area)
+    else
+        self._chunks =
+            ui.Layout():direction(ui.Layout.HORIZONTAL):constraints(
+                {
+                    ui.Constraint.Ratio(0, MANAGER.ratio.all),
+                    ui.Constraint.Ratio(MANAGER.ratio.current + MANAGER.ratio.parent + MANAGER.ratio.preview,
+                        MANAGER.ratio.all),
+                    ui.Constraint.Ratio(0, MANAGER.ratio.all)
+                }
+            ):split(self._area)
+    end
+end
+
